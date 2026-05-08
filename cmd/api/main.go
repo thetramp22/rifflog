6e408f8d1886cc5f76d1f71d71b5be117863a573
Scott Starks <esstarks@hotmail.com>
2026-05-07 19:37:19 -0600

    Add user registration endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
-	"github.com/thetramp22/rifflog/internal/config"
+	"github.com/thetramp22/rifflog/internal/database"
+	"github.com/thetramp22/rifflog/internal/handlers"
+	"github.com/thetramp22/rifflog/internal/repository"
+	"github.com/thetramp22/rifflog/internal/services"
 )
 
 func main() {
@@ -19,40 +20,23 @@ func main() {
 		log.Println("No .env file found")
 	}
 
-	dbConfig, err := config.ParseConfig()
-	if err != nil {
-		log.Fatalf("Unable to parse DB config:", err)
-	}
-
-	var conn *pgx.Conn
-
-	for i := 0; i < 10; i++ {
-		conn, err = pgx.ConnectConfig(context.Background(), dbConfig)
-
-		if err == nil {
-			log.Println("Connected to PostgreSQL")
-			break
-		}
-
-		log.Printf("Database not ready yet... retrying (%d/10)\n", i+1)
-
-		time.Sleep(2 * time.Second)
-	}
-
-	if err != nil {
-		log.Fatal("Unable to connect to database:", err)
-	}
+	conn := database.NewConnection()
 	defer conn.Close(context.Background())
 
 	log.Println("Connected to PostgreSQL")
 
 	router := gin.Default()
 
+	userRepo := repository.NewUserRepository(conn)
+	userService := services.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status": "ok",
 		})
 	})
+	router.POST("/register", userHandler.Register)
 
 	port := os.Getenv("APP_PORT")
 	if port == "" {
