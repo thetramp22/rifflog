736aa17681985b2a1a72c71e030effe13ebdfb13
Scott Starks <esstarks@hotmail.com>
2026-05-11 16:02:36 -0600

    fixed typo

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
-    password_hash NOT NULL TEXT,
+    password_hash TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
