92d226480502d86eb781b6b8ac529850aea2559c
Scott Starks <esstarks@hotmail.com>
2026-05-11 11:45:57 -0600

    fixed type for skill_id from string to int

--- internal/models/sessions_request.go
+++ internal/models/sessions_request.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type SessionsRequest struct {
-	SkillID         string    `json:"skill_id"`
+	SkillID         int       `json:"skill_id"`
 	DurationMinutes int       `json:"duration_minutes"`
 	PracticedAt     time.Time `json:"practiced_at"`
 	Notes           string    `json:"notes"`
