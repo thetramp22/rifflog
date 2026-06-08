e55c325b93b6dd1eccd7e0996cdf28d8977297a8
Scott Starks <esstarks@hotmail.com>
2026-06-08 12:27:51 -0600

    Fix timestamp to timestamptz for user entered practiced_at time

--- migrations/000003_create_practice_sessions_table.up.sql
+++ migrations/000003_create_practice_sessions_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE practice_sessions (
     skill_id INTEGER NOT NULL REFERENCES skills(id),
     duration_minutes INTEGER NOT NULL CHECK (duration_minutes > 0),
     notes TEXT NOT NULL,
-    practiced_at TIMESTAMP NOT NULL,
+    practiced_at TIMESTAMPTZ NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
