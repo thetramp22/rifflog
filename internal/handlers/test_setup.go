e7902a20416c506a64a72c299a04614bb233ddc3
Scott Starks <esstarks@hotmail.com>
2026-06-10 15:13:20 -0600

    Add CreateTestUser helper function for test setups

--- internal/handlers/test_setup.go
+++ internal/handlers/test_setup.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"os"
 	"testing"
 
@@ -9,13 +10,16 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/thetramp22/rifflog/internal/bootstrap"
 	"github.com/thetramp22/rifflog/internal/database"
+	"github.com/thetramp22/rifflog/internal/models"
 	"github.com/thetramp22/rifflog/internal/repository"
 	"github.com/thetramp22/rifflog/internal/services"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type TestApp struct {
-	Router *gin.Engine
-	DB     *pgx.Conn
+	Router   *gin.Engine
+	DB       *pgx.Conn
+	UserRepo *repository.UserRepository
 }
 
 func SetupTestApp(t *testing.T) *TestApp {
@@ -52,7 +56,42 @@ func SetupTestApp(t *testing.T) *TestApp {
 	router.POST("/practice-sessions", practiceSessionHandler.CreatePracticeSession)
 
 	return &TestApp{
-		Router: router,
-		DB:     db,
+		Router:   router,
+		DB:       db,
+		UserRepo: userRepo,
 	}
+}
+
+func CreateTestUser(r *repository.UserRepository, email string, password string) (models.User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	var user models.User
+	query := `
+		INSERT INTO users (email, password_hash)
+		VALUES ($1, $2)
+		RETURNING email, password_hash, id, created_at
+	`
+
+	err = r.DB.QueryRow(
+		context.Background(),
+		query,
+		email,
+		hashedPassword,
+	).Scan(
+		&user.Email,
+		&user.PasswordHash,
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	return user, nil
 }
