519eaf25aed3242b44982b424336d76198fce42e
Scott Starks <esstarks@hotmail.com>
2026-06-11 10:23:48 -0600

    Add test for missing practice duration

--- internal/handlers/practice_session_handler_test.go
+++ internal/handlers/practice_session_handler_test.go
@@ -16,12 +16,12 @@ import (
 )
 
 func TestPracticeSessions(t *testing.T) {
+	// Test App Setup
 	t.Log("creating router")
 	app := SetupTestApp(t)
 	defer app.DB.Close(context.Background())
 
-	t.Log("creating request")
-
+	// Test User Setup
 	email := fmt.Sprintf("test-%d@test.com", time.Now().UnixNano())
 	password := "1234"
 
@@ -31,6 +31,8 @@ func TestPracticeSessions(t *testing.T) {
 	}
 	t.Logf("registered user id=%d", user.ID)
 
+	// Test 1 - valid request
+	t.Log("creating request: valid request")
 	practicedAt := time.Date(
 		2026,
 		time.June,
@@ -81,5 +83,39 @@ func TestPracticeSessions(t *testing.T) {
 	opts := cmpopts.IgnoreFields(models.PracticeSession{}, "ID", "CreatedAt")
 	if diff := cmp.Diff(want, got, opts); diff != "" {
 		t.Errorf("Values mismatch (-want +got):\n%s", diff)
+	}
+
+	// Test 2 - missing duration
+	t.Log("creating request: missing duration")
+	practicedAt = time.Date(
+		2026,
+		time.June,
+		10,
+		14,
+		30,
+		0,
+		0,
+		time.UTC,
+	)
+
+	data = models.CreatePracticeSessionRequest{
+		SkillID:     1,
+		PracticedAt: practicedAt,
+		Notes:       "short practice session",
+		UserID:      user.ID,
+	}
+	jsonBytes, err = json.Marshal(data)
+	if err != nil {
+		t.Fatalf("Failed to marshal JSON: %v", err)
+	}
+	t.Log(string(jsonBytes))
+	req = httptest.NewRequest("POST", "http://localhost:8080/practice-sessions", bytes.NewReader(jsonBytes))
+	w = httptest.NewRecorder()
+
+	t.Log("ServeHTTP call")
+	app.Router.ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusBadRequest {
+		t.Fatalf("expected 400, got %v, body=%s", status, w.Body.String())
 	}
 }
